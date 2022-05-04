dc03ce4e6180ad6712ec96469457e775b340d164
malco96 <76689186+malco96@users.noreply.github.com>
2022-05-04 13:32:04 +0200

    Update test.php

--- test.php
+++ test.php
@@ -2,8 +2,6 @@
 # $ php -f test.php
 
 
-//get data passed from Voice Browser
-
 $dbname = 'heroku_8f7ce219fb9f347';
 $dbuser = 'b342abd3b38c6a';
 $dbpass = 'cbea95c2';
@@ -19,10 +17,13 @@ $result = mysqli_query($link, $test_query);
 if (mysqli_num_rows($result) > 0) {
     // output data of each row
     while($row = mysqli_fetch_assoc($result)) {
-        echo "id: " . $row["farmer_id"]. " - Name: " . $row["name"]. " " . $row["adress"];
+        echo $row["farmer_id"]. " ". $row["name"]." ". $row["adress"]." ". $row["phoneNR"]. $row["union_id"];
+        $pass_address = $row["adress"];
+
     }
 } else {
     echo "0 results";
 }
 $link->close();
 ?>
+
